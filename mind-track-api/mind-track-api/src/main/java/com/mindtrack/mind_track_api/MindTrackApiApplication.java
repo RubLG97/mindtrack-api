dcb9b9e603ab78b8ef2dadfef3c85def0bcb18ea
RubLG97 <rubendam97@gmail.com>
2026-03-07 09:39:20 +0000

    fear:initial project setup - Spring Boot + estructura de paquetes

---
+++ mind-track-api/mind-track-api/src/main/java/com/mindtrack/mind_track_api/MindTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.mindtrack.mind_track_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindTrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindTrackApiApplication.class, args);
+	}
+
+}
---
+++ mind-track-api/mind-track-api/src/test/java/com/mindtrack/mind_track_api/MindTrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtrack.mind_track_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindTrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
